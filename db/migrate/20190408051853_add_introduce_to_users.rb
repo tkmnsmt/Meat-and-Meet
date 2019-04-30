38dfa7a2897118e5a4d1da0c4135ff41efce6b50
梨本拓見 <napple.t@nashimototakuminoMacBook-Pro.local>
2019-04-30 16:13:38 +0900

    migrate reset

--- db/migrate/20190408051853_add_introduce_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIntroduceToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :introduce, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -75,17 +75,22 @@ ActiveRecord::Schema.define(version: 2019_04_26_112545) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "introduce"
+    t.date "birthday"
+    t.string "provider"
+    t.string "uid"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.text "introduce"
-    t.date "birthday"
-    t.string "provider"
-    t.string "uid"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
