942124c6260b3c9b1d1181fc080a95ba44012584
梨本拓見 <napple.t@nashimototakuminoMacBook-Pro.local>
2019-05-23 18:26:11 +0900

    edit pagenation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   def show
     @post = Post.new
     @posts = Post.where(user_id: params[:id]).order("id DESC").page(params[:page]).per(5)
-
     @user=User.find(params[:id])
+    
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
     if @user.id == current_user.id
