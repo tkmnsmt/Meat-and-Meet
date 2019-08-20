591bfd39ccc4bac40e8ee8563bbf02f1bcb1dfcf
梨本拓見 <>
2019-08-20 14:58:46 +0900

    Edit HEAD in application.js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, :only => [:show]
   def show
+    @user = User.find(params[:id])
     @post = Post.new
     @posts = Post.where(user_id: params[:id]).order("id DESC").page(params[:page]).per(10)
-    @user=User.find(params[:id])
     
     @currentUserEntry=Entry.where(user_id: current_user.id)
     @userEntry=Entry.where(user_id: @user.id)
