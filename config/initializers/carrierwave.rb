e4d1a6dbfae15fbef34053360349921ccc43dbf5
梨本拓見 <>
2019-10-03 15:35:34 +0900

    Modify AWS

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,8 +6,8 @@ CarrierWave.configure do |config|
   aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
   aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
   region: 'us-east-1',
-  # host:                  's3.amazonaws.com/geekphoto',             # optional, defaults to nil
-  # endpoint:              'https://s3.us-east-1.amazonaws.com' # optional, defaults to nil
+  host:                  's3.amazonaws.com/meat-and-meet',             # optional, defaults to nil
+  endpoint:              'https://s3.us-east-1.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = 'meat-and-meet'                # required
   config.fog_public     = false                                                 # optional, defaults to true
